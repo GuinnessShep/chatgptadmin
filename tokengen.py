0540ea2d2f21e47e8c7c08715d46acfe59ea9f53
GuinnessShep <108033341+GuinnessShep@users.noreply.github.com>
2023-02-24 18:42:08 -0800

    Update tokengen.py

--- tokengen.py
+++ tokengen.py
@@ -1,39 +1,34 @@
 import time
 import jwt
-
-# Replace with your own values
-CLIENT_ID = "568928957521-m57oil8lfnf2qlq6jnq4blp1ljcq4bc8.apps.googleusercontent.com"
-GCM_SENDER_ID = "568928957521"
-PROJECT_ID = "aichat-4fb19"
-BUNDLE_ID = "com.pb.chatgptmac"
-API_KEY = "AIzaSyCm99fk0vDQlgXMgTM9fzlqXiQWHpdOMKA"
-
-payload = {
-    "aud": API_KEY,
-    "iss": GCM_SENDER_ID,
-    "iat": int(time.time()),
-    "exp": int(time.time()) + 60 * 60, # Token is valid for 1 hour
-    "sub": BUNDLE_ID,
-    "gcm.n.acl": {
-        PROJECT_ID: {
-            "admin": True
-        }
-    }
-}
-# Encode the payload and sign with the server key
-token = jwt.encode(payload, algorithm="HS256")
-# Convert token to string
-token_str = token.decode("utf-8")
-
-# Format the POST data
-post_data = {
-    "token": token_str,
-    "prompt": "Your message"
-}
-
-# Send POST request to chatbot endpoint
 import requests
-response = requests.post("https://us-central1-chatbot-e10c8.cloudfunctions.net/chatbot", json=post_data)
 
-# Print the response
+# Firebase project configuration
+client_id = "568928957521-m57oil8lfnf2qlq6jnq4blp1ljcq4bc8.apps.googleusercontent.com"
+gcm_sender_id = "568928957521"
+project_id = "aichat-4fb19"
+api_key = "AIzaSyCm99fk0vDQlgXMgTM9fzlqXiQWHpdOMKA"
+
+# Function to generate the Firebase token
+def generate_token(client_id, gcm_sender_id, project_id, api_key):
+    now = int(time.time())
+    exp = now + 3600  # Token expires in 1 hour
+    payload = {
+        "aud": project_id,
+        "iss": client_id,
+        "iat": now,
+        "exp": exp,
+        "sub": gcm_sender_id,
+        "android": {"packageName": "com.pb.chatgptmac"}
+    }
+    token = jwt.encode(payload, api_key, algorithm="HS256")
+    return token.decode()
+
+# Generate the Firebase token
+token = generate_token(client_id, gcm_sender_id, project_id, api_key)
+
+# Send a message using the token
+url = "https://us-central1-chatbot-e10c8.cloudfunctions.net/chatbot"
+headers = {"Content-Type": "application/json"}
+data = {"token": token, "prompt": "Your Message"}
+response = requests.post(url, headers=headers, json=data)
 print(response.text)
