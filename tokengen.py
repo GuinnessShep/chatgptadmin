ccc125e6292837e22ed805eea35ba7a74d9ed51a
GuinnessShep <108033341+GuinnessShep@users.noreply.github.com>
2023-02-24 18:38:05 -0800

    Update tokengen.py

--- tokengen.py
+++ tokengen.py
@@ -1,20 +1,39 @@
-import firebase_admin
-from firebase_admin import credentials, auth
+import time
+import jwt
 
-# Load the Firebase configuration and offline token gen data from the JSON file
-import json
-with open('firebase_data.json') as f:
-    data = json.load(f)
+# Replace with your own values
+CLIENT_ID = "568928957521-m57oil8lfnf2qlq6jnq4blp1ljcq4bc8.apps.googleusercontent.com"
+GCM_SENDER_ID = "568928957521"
+PROJECT_ID = "aichat-4fb19"
+BUNDLE_ID = "com.pb.chatgptmac"
+API_KEY = "AIzaSyCm99fk0vDQlgXMgTM9fzlqXiQWHpdOMKA"
 
-# Initialize Firebase Admin SDK with the credentials and configuration
-cred = credentials.Certificate('path/to/serviceAccountKey.json')
-firebase_admin.initialize_app(cred, data['firebase_config'])
+payload = {
+    "aud": API_KEY,
+    "iss": GCM_SENDER_ID,
+    "iat": int(time.time()),
+    "exp": int(time.time()) + 60 * 60, # Token is valid for 1 hour
+    "sub": BUNDLE_ID,
+    "gcm.n.acl": {
+        PROJECT_ID: {
+            "admin": True
+        }
+    }
+}
+# Encode the payload and sign with the server key
+token = jwt.encode(payload, SERVER_KEY, algorithm="HS256")
+# Convert token to string
+token_str = token.decode("utf-8")
 
-# Generate an ID token using the offline token gen data
-custom_token = auth.create_custom_token(uid='user_id', developer_claims=data['offline_token_gen_data'])
+# Format the POST data
+post_data = {
+    "token": token_str,
+    "prompt": "Your message"
+}
 
-# Exchange the custom token for an ID token
-id_token = auth.exchange_custom_token(custom_token)
+# Send POST request to chatbot endpoint
+import requests
+response = requests.post("https://us-central1-chatbot-e10c8.cloudfunctions.net/chatbot", json=post_data)
 
-# Print the generated ID token
-print(id_token)
+# Print the response
+print(response.text)
