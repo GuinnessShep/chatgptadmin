c8b735623c26ae362b2bad601d532260844be101
GuinnessShep <108033341+GuinnessShep@users.noreply.github.com>
2023-02-24 18:48:49 -0800

    Create test.py

---
+++ test.py
@@ -0,0 +1,65 @@
+import requests
+import jwt
+import time
+import json
+
+
+def generate_token(client_id, gcm_sender_id, project_id, api_key):
+    now = int(time.time())
+    exp_seconds = 3600  # Token valid for 1 hour
+    exp_time = now + exp_seconds
+
+    # Create the token payload
+    payload = {
+        "iss": client_id,
+        "sub": client_id,
+        "aud": "https://identitytoolkit.googleapis.com/google.identity.identitytoolkit.v1.IdentityToolkit",
+        "iat": now,
+        "exp": exp_time,
+        "uid": "1",
+        "claims": {
+            "kid": "p256",
+            "alg": "RS256",
+            "typ": "JWT"
+        }
+    }
+
+    # Sign the token using the API key
+    token = jwt.encode(payload, api_key, algorithm="HS256")
+
+    return token.decode()
+
+
+def send_message(token, message):
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {token}"
+    }
+
+    # Create the request body
+    data = {
+        "token": token,
+        "prompt": message
+    }
+
+    # Send the request
+    response = requests.post("https://us-central1-chatbot-e10c8.cloudfunctions.net/chatbot", headers=headers, data=json.dumps(data))
+
+    return response.text
+
+
+if __name__ == "__main__":
+    # Fill in your Firebase Offline Token Gen data here
+    client_id = "568928957521-m57oil8lfnf2qlq6jnq4blp1ljcq4bc8.apps.googleusercontent.com"
+    gcm_sender_id = "568928957521"
+    project_id = "aichat-4fb19"
+    api_key = "AIzaSyCm99fk0vDQlgXMgTM9fzlqXiQWHpdOMKA"
+
+    # Generate a token
+    token = generate_token(client_id, gcm_sender_id, project_id, api_key)
+    print(f"Generated token: {token}")
+
+    # Send a message using the token
+    message = "Hello, ChatGPT!"
+    response = send_message(token, message)
+    print(response)
